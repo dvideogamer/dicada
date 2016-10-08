22bcd05b5e5665d9b0fb412992f4a19aab87862c
Derek <dvideogamer@gmail.com>
2016-10-07 23:46:47 -0400

    refactor

--- src/main/java/com/derek/dao/DayDAOImpl.java
+++ src/main/java/com/derek/dao/DayDAOImpl.java
@@ -2,9 +2,12 @@ package com.derek.dao;
 
 import com.derek.model.Day;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 @Service
@@ -21,8 +24,14 @@ public class DayDAOImpl implements DayDAO {
     @SuppressWarnings("unchecked")
     @Override
     public List<Day> list() {
-        return jdbcTemplate.query(
-            "SELECT * FROM days", (rs, rowNum) -> new Day(rs.getLong("day_id"), rs.getString("name"))
-        );
+        return jdbcTemplate.query("SELECT * FROM days", new DayMapper());
+    }
+
+    private static final class DayMapper implements RowMapper<Day> {
+
+        public Day mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Day day = new Day(rs.getLong("day_id"), rs.getString("name"));
+            return day;
+        }
     }
 }
